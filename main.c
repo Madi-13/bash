73eb7e622e95a63acdfc447fee48d078b33f0636
Madi-13 <56301944+Madi-13@users.noreply.github.com>
2019-10-29 19:43:56 +0600

    Update main.c

--- main.c
+++ main.c
@@ -1,30 +1,27 @@
-#include <unistd.h>
-#include <string.h>
-#include <stdio.h>
-#include <sys/wait.h>
-#include <err.h>
-#include <fcntl.h>
-
-int main(void) {
-	char cmd[10];
-	ssize_t fd = open ( "file.txt ", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR );
-	while (1) {
-		fgets(cmd, 10, stdin);
-		cmd[strlen(cmd) - 1] = '\0';
-		if (!strcmp(cmd, "exit") || !strcmp(cmd, "quit")) {
-			return 0;
-		}
-		pid_t pid = fork();
-		if (pid == 0) {
-			dup2(fd, 1);
-			if (execlp(cmd, cmd, NULL) < 0) {
-				err(STDOUT_FILENO, NULL);
-				return 1;
+int main (void) {
+	char **list, *tmp, sym;
+	int fd, sym_pos;
+	int i = 0;
+	list = get_list();
+	sym_pos = sym_search(list, &sym);
+	while (list && strcmp(list[0], "exit") && strcmp(list[0], "quit")) {
+		if (!sym) {
+			if (fork() > 0) {
+				wait();
+			} else {
+				if (execvp(list[0], list) < 0) {
+					perror("exec trouble");
+					return 1;
+				}
 			}
-			return 0;
 		}
-		wait(NULL);
+		else {
+			redir(list, sym, sym_pos);
+		}
+		clear_list(list);
+		list = get_list();
+		sym_pos = sym_search(list, &sym);
 	}
-	close(fd);
+	clear_list(list);
 	return 0;
 }
