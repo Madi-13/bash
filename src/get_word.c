597fa36846815dedd485e54c2c044ec4069a3d7c
Madi-13 <56301944+Madi-13@users.noreply.github.com>
2019-10-29 19:49:52 +0600

    Update get_word.c

--- src/get_word.c
+++ src/get_word.c
@@ -1,27 +1,13 @@
-char *get_word(char *end)
-{
-	if (*end == '\n') {
-		return NULL;
-	}
+char *get_word (char *end) {
+	char *wrd = NULL;
 	int i = 0;
-	char ch, *word = NULL, *check = NULL;
-	do {
-		ch = getchar();
-		while (!i && (ch == ' ' || ch == '\t')) {
-			ch = getchar();
-			if (ch == '\n') {
-				return NULL;
-			}
-		}
-		check = (char *)realloc(word, (i + 1) * sizeof(char));
-		if (check == NULL) {
-			err(1, NULL);
-		}
-		word = check;
-		word[i] = ch;
+	*end = getchar();
+	while (*end != ' ' && *end != '\t' && *end != '\n') {
+		wrd = realloc(wrd, (i + 1) * sizeof(char));
+		wrd[i] = *end;
 		i++;
-	} while (ch != ' ' && ch != '\t' && ch != '\n');
-	word[i - 1] = '\0';
-	*end = ch;
-	return word;
+		*end = getchar();
+	}
+	wrd[i] = '\0';
+	return wrd;
 }
