f952e535e28b93797dfd3909631bc560fd84dedd
Madi-13 <56301944+Madi-13@users.noreply.github.com>
2019-12-18 03:26:01 +0600

    Update bash.c

--- bash.c
+++ bash.c
@@ -22,17 +22,38 @@ void print() {
   fflush(stdout);
 }
 
+int fon = 0;
+char konv = '\0';
+
+void print() {
+  const char* pwd = getenv("PWD");
+  const char* user = getenv("USER");
+  char host[256];
+  gethostname(host, 256);
+  printf(ANSI_COLOR_GREEN "%s@%s" ANSI_COLOR_RESET ":" ANSI_COLOR_BLUE "%s" ANSI_COLOR_RESET "$ ", user, host, pwd);
+  fflush(stdout);
+}
+
+void print1(char ***cmds) {
+  for (int i = 0; cmds[i] != NULL; i++) {
+      for (int j = 0; cmds[i][j] != NULL; j++) {
+          printf("%s", cmds[i][j]);
+      }
+      printf("\n");
+  }
+}
+
 char *get_word(char *end) {
     char *word = NULL;
-    int i = 0, j = 0;
+    int i = 0, br = 0;
     if (*end == '&') {
-        ampers = 1;
+        fon = 1;
         read(0, end, 1);
         return word;
     }
     if (*end == '|') {
       return word;
-    }   
+    }
     if (*end == '>' || *end == '<') {
         word = (char *)realloc(word, 2);
         if (word == NULL) {
@@ -49,7 +70,7 @@ char *get_word(char *end) {
         return word;
     }
     if (*end == '"') {
-            j = 2;
+            br = 2;
             if (read(0, end, 1) < 0) {
                 perror("read");
                 free(word);
@@ -67,10 +88,10 @@ char *get_word(char *end) {
             }
     }
     while (1) {
-        if ((*end == ' ' || *end == '\n' || *end == '\t') && (j != 2)) {
+        if ((*end == ' ' || *end == '\n' || *end == '\t') && (br != 2)) {
             break;
         }
-        if (*end == '"' && j == 2) {
+        if (*end == '"' && br == 2) {
             *end = ' ';
             break;
         }
@@ -91,6 +112,260 @@ char *get_word(char *end) {
         i++;
     }
     word[i] = '\0';
-    return word;        
+    return word;
 }
 
+void free_3_arr(char ***cmds) {
+    for (int i = 0; cmds[i] != NULL; i++) {
+        for (int j = 0; cmds[i][j] != NULL; j++) {
+            free(cmds[i][j]);
+        }
+        free(cmds[i]);
+    }
+    free(cmds);
+}
+
+char **get_list(char *end) {
+    char **words = NULL;
+    ssize_t err = 0;
+    int i;
+    if (read(1, end, 1) < 0) {
+        perror("read");
+        return NULL;
+    }
+    for (i = 0; *end != '\n' && *end != '|'; i++) {
+        words = (char **)realloc(words, (i + 2) * sizeof(char *));
+        if (words == NULL) {
+            perror("realloc");
+            for (int j = 0; j < i; j++) {
+                free(words[j]);
+            }
+            free(words);
+            return NULL;
+        }
+        while (*end == ' ' || *end == '\t') {
+            err = read(0, end, 1);
+            if (err < 0) {
+                perror("read");
+                for (int j = 0; j < i; j++) {
+                    free(words[j]);
+                }
+                free(words);
+                return NULL;
+            } else if (*end == '\n') {
+                words[i] = NULL;
+                return words;
+            }
+        }
+        words[i] = get_word(end);
+        if (fon == 1) {
+            words[i + 1] = NULL;
+            return words;
+        }
+        if (*end == '|') {
+          words[i + 1] = NULL;
+          return words;
+        }
+        if (words[i] == NULL) {
+            for (int j = 0; j < i; j++) {
+                free(words[j]);
+            }
+            free(words);
+            return NULL;
+        }
+    }
+    if (words != NULL) {
+        words[i] = NULL;
+    }
+    return words;
+}
+
+
+char ***get_cmds(int *n) {
+    char  ***cmds = NULL, end = '\0';
+    int i = 0;
+    while (end != '\n') {
+        cmds = (char ***)realloc(cmds, (i + 2) * sizeof(char **));
+        if (cmds == NULL) {
+            perror("realloc char***");
+            return NULL;
+        }
+        cmds[i] = get_list(&end);
+        if (cmds[i] == NULL && end == '\n') {
+            free_3_arr(cmds);
+            *n = -1;
+            return NULL;
+        }
+        if (cmds[i] == NULL) {
+            perror("get_list");
+            free_3_arr(cmds);
+            return NULL;
+        }
+        i++;
+    }
+    *n = i;
+    if (cmds[i] != NULL) {
+        cmds[i] = NULL;
+    }
+    return cmds;
+}
+
+void free_list(char **list) {
+    for (int i = 0; list[i] != NULL; i++) {
+        free(list[i]);
+    }
+    free(list);
+}
+
+void del_w(char **cmd, int n) {
+    char *word;
+    while (cmd[n + 1] != NULL) {
+        word = cmd[n];
+        cmd[n] = cmd[n + 1];
+        cmd[n + 1] = word;
+        n++;
+    }
+    free(cmd[n]);
+    free(cmd[n + 1]);
+    cmd[n] = NULL;
+}
+
+int redir(char **cmd) {
+    int fd, i = 0;
+    while (cmd[i] != NULL) {
+        if (strcmp(cmd[i], ">") == 0) {
+            fd = open(cmd[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if (fd < 0) {
+                perror("failed to open file after <");
+                exit(1);
+            }
+            dup2(fd, 1);
+            break;
+        } else if (strcmp(cmd[i], "<") == 0) {
+            fd = open(cmd[i + 1], O_RDONLY);
+            if (fd < 0) {
+                perror("failed to open file after >");
+                exit(1);
+            }
+            dup2(fd, 0);
+            break;
+        }
+        i++;
+    }
+    if (cmd[i] != NULL) {
+        del_w(cmd, i);
+        del_w(cmd, i);
+    }
+    return fd;
+}
+
+void do_cmds(char ***cmds, int n) {
+    int fd1, fd2;
+    if (strcmp(cmds[0][0], "cd") == 0) {
+        char *home = getenv("HOME");
+        if (cmds[0][1] == NULL || strcmp(cmds[0][1] , "~") == 0) {
+            chdir(home);
+        } else {
+            chdir(cmds[0][1]);
+        }
+        return;
+    }
+    if (n == 1) {
+        if (fork() == 0) {
+            fd1 = redir(cmds[0]);
+            fd2 = redir(cmds[0]);
+            if (execvp(cmds[0][0], cmds[0]) < 0) {
+                free_3_arr(cmds);
+                perror("exec failed");
+                if (fd1 != 0 && fd1 != 1) {
+                    close(fd1);
+                }
+                if (fd2 != 0 && fd2 != 1) {
+                    close(fd2);
+                }
+                exit(1);
+            }
+        } else {
+            if (!fon) {
+                wait(NULL);
+            }
+            fon = 0;
+            return;
+        }
+    }
+    int pipefd[n - 1][2], pid;
+    for (int i = 0; i < n; i++) {
+        if (i != n - 1) {
+            pipe(pipefd[i]);
+        }
+        if ((pid = fork()) == 0) {
+            if (i == 0) {
+                fd1 = redir(cmds[i]);
+            } else if (i == n - 1) {
+                fd2 = redir(cmds[i]);
+            }
+            if (i != 0) {
+                dup2(pipefd[i - 1][0], 0);
+            }
+            if (i != n - 1) {
+                dup2(pipefd[i][1], 1);
+            }
+            for (int j = 0; j < i + 1; j++) {
+                if (j == n - 1) {
+                    break;
+                }
+                close(pipefd[j][0]);
+                close(pipefd[j][1]);
+            }
+            if (execvp(cmds[i][0], cmds[i]) < 0) {
+                free_3_arr(cmds);
+                perror("exec failed");
+                      close(fd1);
+                      close(fd2);
+                exit(1);
+            }
+        }
+    }
+    if (fon) {
+      n--;
+      fon = 0;
+    }
+    for (int i = 0; i < n; i ++) {
+      if (i != n - 1) {
+        close(pipefd[i][0]);
+        close(pipefd[i][1]);
+      }
+      wait(NULL);
+    }
+    if (fd1 != 0 && fd1 != 1) {
+        close(fd1);
+    }
+    if (fd2 != 0 && fd2 != 1) {
+        close(fd2);
+    }
+}
+
+
+int main(void) {
+    char ***cmds = NULL;
+    int n = 0;
+    while (1) {
+        print();
+        cmds = get_cmds(&n);
+        if (n == -1) {
+          continue;
+        }
+        if (cmds == NULL) {
+            perror("something went wrong");
+            return 1;
+        }
+        if (strcmp(cmds[0][0], "quit") == 0 ||
+            strcmp(cmds[0][0], "exit") == 0) {
+            free_3_arr(cmds);
+            break;
+        }
+        do_cmds(cmds, n);
+        free_3_arr(cmds);
+    }
+    return 0;
+}
